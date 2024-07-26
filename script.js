4baae40067f63d09f2020e7c01e53745e18ee067
KolbyG1 <kolbygilkey12@gmail.com>
2024-07-26 18:20:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Name: Kolby Gilkey 
+// Date: 07/26/2024
+// File Name: index.html
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Example JavaScript functionality
+    console.log('Fitness website loaded');
+});
